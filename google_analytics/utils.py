3a3b1033db9c7342b02c6fd357ef8259c6a42d56
Adrian Altenhoff <adrian.altenhoff@inf.ethz.ch>
2017-10-11 13:12:01 +0200

    use alternative visitor id

--- google_analytics/utils.py
+++ google_analytics/utils.py
@@ -2,6 +2,7 @@ import random
 import sys
 import time
 import uuid
+import hashlib
 
 from django.conf import settings
 from django.utils.translation import get_language_from_request
@@ -19,16 +20,18 @@ COOKIE_USER_PERSISTENCE = 63072000
 CAMPAIGN_PARAMS_KEY = 'ga_campaign_params'
 
 
-def get_visitor_id(guid, cookie):
+def get_visitor_id(cookie, client_ip, request):
     """Generate a visitor id for this hit.
     If there is a visitor id in the cookie, use that, otherwise
-    use the guid if we have one, otherwise use a random number.
+    use the authenticated user or as a last resort the IP.
     """
     if cookie:
         return cookie
-    if guid:
-        # create the visitor id using the guid.
-        cid = guid
+    if request.user.is_is_authenticated:
+        # create the visitor id from the username
+        cid = hashlib.md5(request.user.username.encode('utf-8')).hexdigest()
+    elif client_ip:
+        cid = hashlib.md5(client_ip.encode('utf-8')).hexdigest()
     else:
         # otherwise this is a new user, create a new random id.
         cid = str(uuid.uuid4())
@@ -38,10 +41,15 @@ def get_visitor_id(guid, cookie):
 def set_cookie(params, response):
     COOKIE_USER_PERSISTENCE = params.get('COOKIE_USER_PERSISTENCE')
     COOKIE_PATH = params.get('COOKIE_PATH')
-    visitor_id = params.get('visitor_id')
+    # use visitor_id as cookie value unless it is based on the IP only.
+    # then generate a new value (if cookie is sent next time, we have a 
+    # better visitor id!
+    visitor_id = params.visitor_id
+    client_ip = params.client_ip
+    if visitor_id == hashlib.md5(client_ip.encode('utf-8')).hexdigest():
+        visitor_id = str(uuid.uuid4())
 
     time_tup = time.localtime(time.time() + COOKIE_USER_PERSISTENCE)
-
     # always try and add the cookie to the response
     response.set_cookie(
         COOKIE_NAME,
@@ -67,16 +75,16 @@ def build_ga_params(
     # get the path from the referer header
     path = path or request.GET.get('p', '/')
 
-    # try and get visitor cookie from the request
-    user_agent = meta.get('HTTP_USER_AGENT', 'Unknown')
-    cookie = request.COOKIES.get(COOKIE_NAME)
-    visitor_id = get_visitor_id(meta.get('HTTP_X_DCMGUID', ''), cookie)
-
     # get client ip address
     if 'HTTP_X_FORWARDED_FOR' in meta:
         client_ip = meta.get('HTTP_X_FORWARDED_FOR', '')
     else:
         client_ip = meta.get('REMOTE_ADDR', '')
+
+    # try and get visitor cookie from the request
+    user_agent = meta.get('HTTP_USER_AGENT', 'Unknown')
+    cookie = request.COOKIES.get(COOKIE_NAME)
+    visitor_id = get_visitor_id(cookie, client_ip, request)
 
     # build the parameter collection
     params = {
@@ -128,6 +136,7 @@ def build_ga_params(
             'user_agent': user_agent,
             'language': locale or settings.LANGUAGE_CODE,
             'visitor_id': visitor_id,
+            'client_ip': client_ip,
             'COOKIE_USER_PERSISTENCE': COOKIE_USER_PERSISTENCE,
             'COOKIE_NAME': COOKIE_NAME,
             'COOKIE_PATH': COOKIE_PATH,
